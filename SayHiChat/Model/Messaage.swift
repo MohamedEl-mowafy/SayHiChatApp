14dd40fdf3b7e2ee23ca45ae462afe96349dce6c
MohamedEl-mowafy <eng.mohamedelmwafy@gmail.com>
2022-02-02 20:00:28 +0200

    final

---
+++ SayHiChat/Model/Messaage.swift
@@ -0,0 +1,8 @@
+//
+//  Messaage.swift
+//  SayHiChat
+//
+//  Created by Mohamed Elmowafy on 02/02/2022.
+//
+
+import Foundation
---
+++ SayHiChat/View/MessageCell.swift
@@ -0,0 +1,23 @@
+//
+//  MessageCell.swift
+//  SayHiChat
+//
+//  Created by Mohamed Elmowafy on 02/02/2022.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
